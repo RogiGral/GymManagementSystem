9b5e05219d0b7cb301309d8264c28f0b43c7163b
DIR\igor.gralak <igor.gralak>
2024-10-15 21:36:29 +0200

    Added notification again -> prev deleted cause when changing version caused a lot of bugs

--- frontend-angular/gymApp/src/app/app.module.ts
+++ frontend-angular/gymApp/src/app/app.module.ts
@@ -24,7 +24,48 @@ import {ScoreService} from "./service/score.service";
 import {NgxPaginationModule} from "ngx-pagination";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {NotifierModule, NotifierOptions} from "angular-notifier";
 
+const customNotifierOptions: NotifierOptions = {
+  position: {
+    horizontal: {
+      position: 'left',
+      distance: 12
+    },
+    vertical: {
+      position: 'top',
+      distance: 12,
+      gap: 10
+    }
+  },
+  theme: 'material',
+  behaviour: {
+    autoHide: 5000,
+    onClick: 'hide',
+    onMouseover: 'pauseAutoHide',
+    showDismissButton: true,
+    stacking: 4
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: 'slide',
+      speed: 300,
+      easing: 'ease'
+    },
+    hide: {
+      preset: 'fade',
+      speed: 300,
+      easing: 'ease',
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: 'ease'
+    },
+    overlap: 150
+  }
+};
 
 @NgModule({
   declarations: [
@@ -45,6 +86,7 @@ import {TranslateHttpLoader} from "@ngx-translate/http-loader";
     HttpClientModule,
     FormsModule,
     NgxPaginationModule,
+    NotifierModule.withConfig(customNotifierOptions),
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -53,6 +95,7 @@ import {TranslateHttpLoader} from "@ngx-translate/http-loader";
       },
     }),
   ],
+  exports: [NotifierModule],
   providers: [NotificationService, AuthenticationService, UserService,WorkoutService,MembershipService,ScoreService,
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
--- frontend-angular/gymApp/src/app/service/notification.service.ts
+++ frontend-angular/gymApp/src/app/service/notification.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
+import {NotifierService} from "angular-notifier";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  private readonly notifier: NotifierService;
+
+  constructor(private notifierService: NotifierService) {
+
+    this.notifier = notifierService;
+  }
 
   public notify(type: string, message: string){
-    console.log(type + ": " + message)
+    this.notifier.notify(type,message);
   }
 }
